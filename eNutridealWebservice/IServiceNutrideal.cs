08f3347899f7c705d857e65e3a0cb5c6b56cfe26
jo-Ribeiro <44573747+jo-Ribeiro@users.noreply.github.com>
2019-01-10 19:31:00 +0000

    Add files via upload

---
+++ eNutridealWebservice/IServiceNutrideal.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace eNutridealWebservice
+{
+
+    [ServiceContract]
+    public interface IServiceNutrideal
+    {
+        // calculadora peso ideal
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "pesoideal")]
+        [Description("Calcula o peso ideal")]
+        double CalcularPesoIdeal(Dados d);
+
+        // Calculadora Calorias
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "calorias")]
+        [Description("Calcula as calorias")]
+        double CalculadoraCalorias(Dados d);
+
+
+        // Get refeicao
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/refeicoes")]
+        [Description("Todas as refiecoes apresentadas na base de dados")]
+        List<Refeicao> GetRefeicoes();
+
+        // add refeicao
+
+        [OperationContract]
+        [WebInvoke(Method = "Post", UriTemplate = "/refeicoes")]
+        [Description("Adicionar todos as refeicoes")]
+        void AddRefeicao(Refeicao refeicao);
+
+      
+    }
+
+    [DataContract]
+    public class Dados
+    {
+        [DataMember]
+        public int Idade { get; set; }
+
+        [DataMember]
+        public double Altura { get; set; }
+
+        [DataMember]
+        public double Peso { get; set; }
+
+        [DataMember]
+        public String Sexo { get; set; }
+
+    }
+
+    [DataContract]
+    public class Refeicao
+    {
+        public Refeicao(string nomeRefeicao, string item, string quantidade, string calorias)
+        {
+            this.NomeRefeicao = nomeRefeicao;
+            this.Item = item;
+            this.Quantidade = quantidade;
+            this.Calorias = calorias;
+        }
+
+        [DataMember]
+        public string NomeRefeicao { get; set; }
+        [DataMember]
+        public string Item { get; set; }
+        [DataMember]
+        public string Quantidade { get; set; }
+        [DataMember]
+        public string Calorias { get; set; }
+
+        public override string ToString()
+        {
+            string res = string.Empty;
+            res += "Nome da Refeição: " + NomeRefeicao + Environment.NewLine;
+            res += "Item: " + Item + Environment.NewLine;
+            res += "Quantidade: " + Quantidade + Environment.NewLine;
+            res += "Calorias: " + Calorias + Environment.NewLine;
+            return res;
+
+        }
+    }
+}
---
+++ eNutridealWebservice/ServiceNutrideal.svc.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Hosting;
+using System.Xml;
+
+namespace eNutridealWebservice
+{
+    public class ServiceNutrideal : IServiceNutrideal
+    {
+        public static string FILEPATH;
+
+        public ServiceNutrideal()
+        {
+            FILEPATH = Path.Combine(HostingEnvironment.ApplicationPhysicalPath, "App_Data");
+        }
+
+        public void CalcularPesoIdeal(Dados d)
+        {
+
+        }
+
+        public void CalculadoraCalorias(Dados d)
+        {
+               
+        }
+
+
+
+        public List<Refeicao> GetRefeicoes()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            List<Refeicao> refeicoes = new List<Refeicao>();
+            XmlNodeList refeicaoNodes = doc.SelectNodes("/refeicao");
+
+            foreach (XmlNode refeicaoNode in refeicaoNodes)
+            {
+                XmlNode nomeRefeicaoNode = refeicaoNode.SelectSingleNode("nomeRestaurante");
+                XmlNode itemNode = refeicaoNode.SelectSingleNode("item");
+                XmlNode quantidadeNode = refeicaoNode.SelectSingleNode("quantidade");
+                XmlNode caloriasNode = refeicaoNode.SelectSingleNode("calorias");
+
+                Refeicao refeicao = new Refeicao(
+                    nomeRefeicaoNode.InnerText, itemNode.InnerText,
+                    quantidadeNode.InnerText, caloriasNode.InnerText);
+
+                refeicoes.Add(refeicao);
+
+            }
+            return refeicoes;
+        }
+
+
+        public void AddRefeicao(Refeicao refeicao)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+
+            XmlNode refeicoesNode = doc.SelectSingleNode("/refeicao");
+
+            XmlElement refeicaoNode = doc.CreateElement("Refeicao");
+            XmlElement nomeRefeicaoNode = doc.CreateElement("nomeRefeicao");
+            nomeRefeicaoNode.InnerText = refeicao.NomeRefeicao;
+            refeicaoNode.AppendChild(nomeRefeicaoNode);
+            XmlElement itemNode = doc.CreateElement("item");
+            itemNode.InnerText = refeicao.Item;
+            refeicaoNode.AppendChild(itemNode);
+            XmlElement quantidadeNode = doc.CreateElement("nomeRefeicao");
+            quantidadeNode.InnerText = refeicao.Quantidade;
+            refeicaoNode.AppendChild(quantidadeNode);
+            XmlElement caloriasNode = doc.CreateElement("nomeRefeicao");
+            caloriasNode.InnerText = refeicao.Calorias;
+            refeicaoNode.AppendChild(caloriasNode);
+
+            doc.Save(FILEPATH);
+        }
+
+
+
+    }
+}
+
+
